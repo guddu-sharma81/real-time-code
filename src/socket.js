6f2459a73b5799b436af4a668e4854b95cf74326
Guddu <gk5253448@gmail.com>
2024-11-18 19:32:05 +0530

    rmove build

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
-    const options = await {
+    const options = {
         'force new connection': true,
         reconnectionAttempt: 'Infinity',
         timeout: 10000,
